ea717cdc8b6c83ce84c4d6a14121bd2273c064f8
vujadinjanjic <57356499+vujadinjanjic@users.noreply.github.com>
2020-03-10 21:00:03 +0100

    End of 6 section

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 
 @Component({
@@ -10,28 +11,28 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
  model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
-  login(){
+  login() {
     this.authService.login(this.model).subscribe( next => {
-      console.log('Logged in succesfully');
+      console.log('Logged succesfully');
+      // this.alertify.succes('Logged in succesfully');
   }, error => {
-      console.log(error);
+      console.log('Error while logging..')
+      // this.alertify.error(error);
     });
   }
 
-  loggedIn()
-  {
-    const token = localStorage.getItem('token');
-    return !! token;
+  loggedIn() {
+   return this.authService.loggedIn();
   }
 
-  logout()
-  {
+  logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    console.log('Logout successfuly.')
+    // this.alertify.message('logged out');
   }
 
 }
